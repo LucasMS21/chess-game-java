fdcfc797f201895f848c3baa6597e920b16e0a3b
LucasMS21 <lucasedu.6a@gmail.com>
2026-08-07 16:58:34 -0300

    Placing initial setup pieces on the board

--- src/main/java/chesslayer/ChessMatch.java
+++ src/main/java/chesslayer/ChessMatch.java
@@ -1,6 +1,9 @@
 package chesslayer;
 
 import boardgame.Board;
+import boardgame.Position;
+import chesslayer.pieces.King;
+import chesslayer.pieces.Rook;
 
 public class ChessMatch {
 
@@ -15,6 +18,7 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -25,6 +29,13 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+        board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+
     }
 
 
