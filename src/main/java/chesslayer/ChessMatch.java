34afc21143e1f6391b889b8f9161acdabc02e67c
LucasMS21 <lucasedu.6a@gmail.com>
2026-08-10 15:32:00 -0300

    ChessException and ChessPosition fix

--- src/main/java/chesslayer/ChessMatch.java
+++ src/main/java/chesslayer/ChessMatch.java
@@ -36,9 +36,9 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
-        board.placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-        board.placeNewPiece('e', 8, new King(board, Color.BLACK));
-        board.placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
 
     }
 
