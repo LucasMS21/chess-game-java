f84735f09a905fbd6c48141df5a1918e28850d35
LucasMS21 <lucasedu.6a@gmail.com>
2026-08-11 16:55:24 -0300

    Trying an implementation

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -1,6 +1,10 @@
 package boardgame;
 
-public class Piece {
+import chesslayer.ChessPiece;
+
+import java.util.Arrays;
+
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -13,7 +17,30 @@ public class Piece {
         return board;
     }
 
+    public abstract boolean[][] possibleMoves();
 
+
+    public boolean possibleMove(Position position) {
+        int row = position.getRow();
+        int column = position.getColumn();
+        boolean matAux[][] = possibleMoves();
+
+        return matAux[row][column];
+    }
+
+
+    public boolean isThereAnyPossibleMove() {
+        boolean matAux[][] = possibleMoves();
+        for (int i = 0; i < matAux.length; i++) {
+            for (int j = 0; j < matAux.length; j++) {
+                if(matAux[i][j] == true){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
 
 
 }
